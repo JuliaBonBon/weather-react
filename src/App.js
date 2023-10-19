b92c8a90d3db6251286a3647742ce6208b6bc873
JuliaBonBon <juliabonbon1@gmail.com>
2023-10-18 19:56:18 -0600

    Added footer instead of element small and added some CSS to it

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Search />
-      <small>
-        <a
-          href="https://github.com/JuliaBonBon/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>
-      </small>{" "}
-      by Yuliia Drivinska
+      <div className="container">
+        <h1>Weather App</h1>
+        <Search />
+        <footer>
+          <a
+            href="https://github.com/JuliaBonBon/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code {""}
+          </a>
+          {""}
+          by Yuliia Drivinska
+        </footer>
+      </div>
     </div>
   );
 }
