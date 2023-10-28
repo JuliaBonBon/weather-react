7c85eadc57fa45fc5e741fa5c7956671e00f3d63
JuliaBonBon <juliabonbon1@gmail.com>
2023-10-28 13:46:15 -0400

    Added new component WeatherIcons, worked on CSS, installed Animated react weather

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,11 +15,7 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/
-partly_cloudy.png"
-            alt="Nostly Cloudy"
-          />
+          <WeatherIcon code={props.data.icon} />
           <span className="temperature">
             {Math.round(props.data.temperature)}
           </span>{" "}
