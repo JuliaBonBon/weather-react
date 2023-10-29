a2a0553879c07a7d5c47e697164df2b37df68850
JuliaBonBon <juliabonbon1@gmail.com>
2023-10-29 13:57:06 -0400

    Added new WeatherTemperature component

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#C7DBFF"
-      size={53}
+      size={55}
       animate={true}
     />
   );
