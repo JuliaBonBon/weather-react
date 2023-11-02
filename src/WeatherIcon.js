d7d610df9e0076ab835f4e5e050a84e78808ade8
JuliaBonBon <juliabonbon1@gmail.com>
2023-11-01 21:35:02 -0600

    Added new ForecastDay component . changes in WeatherForecast component, updated CSS

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#2B7FD6"
+      color="#4190e0"
       size={props.size}
       animate={true}
     />
